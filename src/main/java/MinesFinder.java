8f1aae77094fd9907d042510ff7145f33eb1e039
GuilhermeGuilherme <2211087@my.ipleiria.pt>
2023-03-07 12:29:07 +0000

    minesfinder btnSair

--- src/main/java/MinesFinder.java
+++ src/main/java/MinesFinder.java
@@ -1,18 +1,27 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class MinesFinder extends JFrame {
     private JPanel Principal;
     private JButton button1;
     private JButton button2;
     private JButton button3;
-    private JButton button4;
+    private JButton btnSair;
 
     public MinesFinder(String title) {
         super(title);
 
         setContentPane(Principal); //
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Causes this Window to be sized to fit the preferred size and layouts of its subcomponents.
         pack();
+
+        btnSair.addActionListener(this::btnSairActionPerformed);
+    }
+
+    private void btnSairActionPerformed(ActionEvent e) {
+        System.exit(0);
     }
 
     public static void main(String[] args) {
